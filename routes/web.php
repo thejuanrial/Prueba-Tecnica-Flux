78fcc2591bc130c1b5bc37ee247a12f23b8ffb19
rial <thejuanrial@gmail.com>
2025-12-03 11:05:49 -0400

    Creacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
-<?php
+// rutas
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController
+use Illimunate\Support\Facades\Route;
 
+
+// ruta de recurso para el crud
+Route::resource('productos', ProductoController::class),
+
+//establecer la pagina de inicio  como la lista de productos
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('productos.index');
 });
