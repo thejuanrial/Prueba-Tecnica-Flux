cc25cbb6b9e5b2341b2d27db9450d8ba61d6098b
rial <thejuanrial@gmail.com>
2025-12-03 10:35:24 -0400

    Configuración de modelo

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -1,10 +1,19 @@
-<?php
+// productos
 
-namespace App\Models;
+namespace App\Models
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'precio_costo',
+        'precio_venta',
+        'stock',
+    ];
 }
